5e37aa6eb767059ed6aea6fbf859178f2438e0bc
hjh0915 <623299157@qq.com>
2020-11-09 11:44:04 +0800

    修改侧滑栏功能

--- app/src/main/java/com/work/transportapplication/DrawerActivity.java
+++ app/src/main/java/com/work/transportapplication/DrawerActivity.java
@@ -1,63 +1,68 @@
 package com.work.transportapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Menu;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.NavigationView;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import android.view.MenuItem;
+import android.view.View;
 
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
+import android.widget.ImageView;
 
 public class DrawerActivity extends AppCompatActivity {
-
-    private AppBarConfiguration mAppBarConfiguration;
+    private DrawerLayout drawerLayout;
+    //侧滑菜单栏
+    private NavigationView navigationView;
+    //标题栏-侧滑-按钮
+    ImageView menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        //开启手势滑动打开侧滑菜单栏，如果要关闭手势滑动，将后面的UNLOCKED替换成LOCKED_CLOSED 即可
+        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+
+        /**
+         * 侧滑菜单item点击事件
+         */
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onNavigationItemSelected( MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        Intent intent1 = new Intent();
+                        intent1.setClass(DrawerActivity.this, HistogramActivity.class);
+                        startActivity(intent1);
+                        //此处不能加Finsh杀死界面否则点击返回会退出程序而不是回到主界面
+                        break;
+                    case R.id.nav_gallery:
+                        Intent intent2 = new Intent();
+                        intent2.setClass(DrawerActivity.this, PieActivity.class);
+                        startActivity(intent2);
+                        break;
+                    case R.id.nav_slideshow:
+                        Intent intent3 = new Intent();
+                        intent3.setClass(DrawerActivity.this, MapActivity.class);
+                        startActivity(intent3);
+                        break;
+                    case R.id.nav_line:
+                        Intent intent4 = new Intent();
+                        intent4.setClass(DrawerActivity.this, LineActivity.class);
+                        startActivity(intent4);
+                        break;
+                    default:
+                        break;
+                }
+                drawerLayout.closeDrawer(navigationView);
+                return true;
             }
         });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.drawer, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
